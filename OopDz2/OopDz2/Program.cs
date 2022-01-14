83d63e3b77517988b2665ec18bb29de0a4568c1b
Moreart1 <morearti1726263@yandex.ru>
2022-01-14 13:49:51 +0300

    Сделал 1 задание

---
+++ OopDz2/OopDz2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OopDz2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bankCurrent = new Bank("14997GTY789", 554875, AccountType.Current);
+            Console.WriteLine($"{bankCurrent}\n");
+
+            Bank bankSavings = new Bank("1554EFG567", 192540, AccountType.Savings);
+            Console.WriteLine(bankSavings);
+        }
+    }
+}
