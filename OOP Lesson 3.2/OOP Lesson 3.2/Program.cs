6c632df4cdfd6c5e536210c3ecd0723cdc00102a
Moreart1 <morearti1726263@yandex.ru>
2022-01-18 18:32:51 +0300

    Сделал реверс Задание 3.2

--- OOP Lesson 3.2/OOP Lesson 3.2/Program.cs
+++ OOP Lesson 3.2/OOP Lesson 3.2/Program.cs
@@ -4,9 +4,26 @@ namespace OOP_Lesson_3._2
 {
     class Program
     {
+        static string Rev(string str)
+        {
+
+            char[] reverse = str.ToCharArray();
+
+            Array.Reverse(reverse);
+
+            return new string(reverse);
+
+        }
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Введите строку:");
+
+            string line = Console.ReadLine();
+
+            line = Rev(line);
+
+            Console.WriteLine(line);
         }
     }
-}
+    }
+
