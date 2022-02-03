f0d138e9812d2508d863548370da04f70b11dd42
Moreart1 <morearti1726263@yandex.ru>
2022-02-03 17:03:54 +0300

    Сделал ДЗ 7

---
+++ OOP LESSON 7/OOP LESSON 7/ACoder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_LESSON_7
+{
+    class ACoder:ICoder
+    {       
+        /// Защифровываем наш алфавит со сдвигом на 1 букву вперед     
+        /// <param name="toEncode"></param>
+        /// <returns></returns>     
+        public string Encode(string toEncode)
+        {
+            var data = toEncode.AsSpan(); //Создаем новый диапазон строки и помещаем в переменную
+            var sb = new StringBuilder(toEncode.Length); //длина строки
+            foreach (var symbol in data)
+            {
+                sb.Append((char)(symbol + 1)); //добавляем  подстроку в переменную 
+            }
+
+            return sb.ToString();
+        }
+       
+        /// Расшифровываем наш алфавит       
+        /// <param name="toDecode"></param>
+        /// <returns></returns>      
+        public string Decode(string toDecode)
+        {
+            var data = toDecode.AsSpan();
+            var sb = new StringBuilder(toDecode.Length);
+            foreach (var symbol in data)
+            {
+                sb.Append((char)(symbol));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ OOP LESSON 7/OOP LESSON 7/BCoder.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_LESSON_7
+{
+    class BCoder : ICoder
+    {
+        private static readonly int[] LowerCaseRange = { 'a', 'z' }; // массив нижнего регистра
+        private static readonly int[] UpperCaseRange = { 'A', 'Z' }; // массив верхнего регистра
+
+        private static bool IsLowerCase(char symbol) => symbol >= LowerCaseRange[0] && symbol <= LowerCaseRange[1];
+
+        private static bool IsUpperCase(char symbol) => symbol >= UpperCaseRange[0] && symbol <= UpperCaseRange[1];
+
+        /// <summary>
+        /// Зашифровываем наш алфавит выполняя замену каждой буквы, стоящей в алфавите на i-й позиции, на букву того же регистра, расположенную в алфавите на i-й позиции с конца алфавита.
+        /// </summary>
+        /// <param name="toEncode"></param>
+        /// <returns></returns>
+        public string Encode(string toEncode)
+        {
+            var data = toEncode.AsSpan();
+            var sb = new StringBuilder(toEncode.Length);
+
+            foreach (var symbol in data)
+            {
+                if (IsLowerCase(symbol))
+                {
+                    var rangeFromStart = symbol - LowerCaseRange[0];
+                    if (rangeFromStart == 0)
+                    {
+                        sb.Append((char)LowerCaseRange[1]);
+                        continue;
+                    }
+                    sb.Append((char)(LowerCaseRange[1] - rangeFromStart));
+                    continue;
+                }
+
+
+                if (IsUpperCase(symbol))
+                {
+                    var rangeFromStart = symbol - UpperCaseRange[0];
+                    if (rangeFromStart == 0)
+                    {
+                        sb.Append((char)UpperCaseRange[1]);
+                        continue;
+                    }
+                    sb.Append((char)(UpperCaseRange[1] - rangeFromStart));
+                }
+            }
+
+            return sb.ToString();
+        }
+
+       /// <summary>
+       /// Зашифровываем наш алфавит
+       /// </summary>
+       /// <param name="toDecode"></param>
+       /// <returns></returns>
+        public string Decode(string toDecode)
+        {
+            var data = toDecode.AsSpan();
+            var sb = new StringBuilder(toDecode.Length);
+
+            foreach (var symbol in data)
+            {
+                if (IsLowerCase(symbol))
+                {
+                    var rangeFromEnd = LowerCaseRange[1] - symbol;
+                    if (rangeFromEnd == 0)
+                    {
+                        sb.Append((char)LowerCaseRange[0]);
+                        continue;
+                    }
+                    sb.Append((char)(LowerCaseRange[0] + rangeFromEnd));
+                    continue;
+                }
+
+                if (IsUpperCase(symbol))
+                {
+                    var rangeFromEnd = UpperCaseRange[1] - symbol;
+                    if (rangeFromEnd == 0)
+                    {
+                        sb.Append((char)UpperCaseRange[0]);
+                        continue;
+                    }
+                    sb.Append((char)(UpperCaseRange[0] + rangeFromEnd));
+                }
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
